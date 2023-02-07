b916cfba096d3bb00b4da4c9f805973e82b15002
Alex <ajoshua14@icloud.com>
2023-02-07 13:51:09 -0800

    Add Gson to project and implement an ArrayList to hold blockchain

--- app/src/main/java/com/obsidianchain/ObsidianChain.java
+++ app/src/main/java/com/obsidianchain/ObsidianChain.java
@@ -4,22 +4,27 @@
 package com.obsidianchain;
 
 import com.obsidianchain.components.*;
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
+
 
 public class ObsidianChain {
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
     public String getGreeting() {
         return "Hello World!";
     }
 
     public void createSomeBlocks() {
-        Block genesisBlock = new Block("Hi im the first block", "0");
-		System.out.println("Hash for block 1 : " + genesisBlock.hash);
+        blockchain.add(new Block("Hi im the first block", "0"));
 		
-		Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
-		System.out.println("Hash for block 2 : " + secondBlock.hash);
+		blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).hash));
 		
-		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
-		System.out.println("Hash for block 3 : " + thirdBlock.hash);
+		blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).hash));
+		
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
     }
 
     public static void main(String[] args) {
